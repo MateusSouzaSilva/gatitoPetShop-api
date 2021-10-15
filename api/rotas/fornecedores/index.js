031f9866c2ab09674be1cc61fb0e59f75b4c139e
Mateus Souza <mateus_souza11@hotmail.com.br>
2021-10-15 16:51:42 -0300

    criando o metodo atualizar e remover

--- api/rotas/fornecedores/index.js
+++ api/rotas/fornecedores/index.js
@@ -8,6 +8,7 @@ roteador.get('/', async (requisicao, resposta) => {
         JSON.stringify(resultados)
     );
 });
+
 roteador.post('/', async (requisicao, resposta) => {
     const dadosRecebidos = requisicao.body;
     const fornecedor = new Fornecedor(dadosRecebidos);
@@ -15,5 +16,52 @@ roteador.post('/', async (requisicao, resposta) => {
     resposta.send(
         JSON.stringify(fornecedor)
     );
+});
+
+roteador.get('/:idFornecedor', async (requisicao, resposta) => {
+    try {
+        const id = requisicao.params.idFornecedor;
+        const fornecedor = new Fornecedor({ id: id });
+        await fornecedor.carregar();
+        resposta.send(
+            JSON.stringify(fornecedor)
+        );
+    } catch (erro) {
+        resposta.send(
+            JSON.stringify({
+                mensagem: erro.message
+            })
+        );
+    }
+});
+
+roteador.put('/:idFornecedor', async (requisicao, resposta) => {
+    try {
+    const id = requisicao.params.idFornecedor;
+    const dadosRecebidos = requisicao.body;
+    const dados = Object.assign({}, dadosRecebidos, { id: id});
+    const fornecedor = new Fornecedor(dados);
+    await fornecedor.atualizar();
+    resposta.end();
+    } catch (erro) {
+        resposta.send(JSON.stringify({
+            mensagem: erro.message
+        }));
+    }
+});
+roteador.delete('/:idFornecedor', async (requisicao, resposta) => {
+    try {
+        const id = requisicao.params.idFornecedor;
+        const fornecedor = new Fornecedor({ id: id });
+        await fornecedor.carregar();
+        await fornecedor.remover();
+        resposta.end();
+    } catch (erro) {
+        resposta.send(
+            JSON.stringify({
+                mensagem: erro.message
+            })
+        );
+    }
 });
 module.exports = roteador;
